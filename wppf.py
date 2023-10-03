312b482dba1322c8b2e2ef88db5fe126e5220ce8
SAEED HASSAN <saeedhisbani@yahoo.co.uk>
2023-10-03 17:50:30 +0500

    Inserted unique year, month per employee on 12 months for each employee

--- wppf.py
+++ wppf.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
 import os
-#import subprocess
-#result = subprocess.run(["ls -l"], shell=True, capture_output=True, text=True)
-#print(result.stdout)
-#input()
+def  makedic():
+    # make dict for each emp and intionalize
+    for mdate in mdates:
+        empdic[str(mdate[0])+str(mdate[1])]=([0,0])
+####################################################
 
-def finddays(code,mfile):
-    mdays,pdays = 0,0
-    with open('data/'+mfile) as txtf:
-        for row in txtf:
-            r=row.split('\t')
-            if code == r[0]:
-                print(r[2],r[3])
-                return r[2],r[3]
-        return 0,0
 files=[file for file in os.listdir('data') if '.txt' in file]
 mcodes=[]
 mdates=[]
@@ -25,24 +17,35 @@ for file in files:
                 mcodes.append(r[0])
                 #r[5] is month r[6] is year
                 mdates.append((int(r[5]),int(r[4]),file))
-    print('The End of file',file)
 
 mdates=set(mdates) # to make unique
 mdates=list(mdates)   
 mdates.sort()
+
+mcodes=set(mcodes) # to make unique
+mcodes=list(mcodes)   
+mcodes.sort()
+
 for code in mcodes:
-    print(code,sep='\t')
+    tdays=0
+    empdic={}
+    makedic()
     for mdate in mdates:
         myear  = mdate[0]
         mmonth = mdate[1]
         mfile  = mdate[2]
-        mdays,pdays = finddays(code,mfile)
-        print(mdate[0],mdate[1],mdate[2],sep='\t')
-    print() ## LF at end of each emp code
-    input()
-print(mdates)
-#print('LEN Mdates',len(mdates),'Len of Files',len(files))
-mcodes=set(mcodes) # to make unique
-mcodes=list(mcodes)   
-mcodes.sort()
-print('T codes',len(mcodes))
+        with open('data/'+mfile) as txtf:    
+            for row in txtf:
+                r=row.split('\t')
+                if r[0] == code:
+                    empdic[str(mdate[0])+str(mdate[1])][0]+=int(r[2])
+                    empdic[str(mdate[0])+str(mdate[1])][1]+=int(r[3])
+                    tdays +=int(r[2])+int(r[3])
+                    break
+
+    print(code,end='\t')
+    for row in empdic:
+        print(empdic[row][0],empdic[row][1],sep='\t',end='\t')
+    print(tdays) ## LF at end of each emp code
+
+print('Total Employees codes',len(mcodes))
